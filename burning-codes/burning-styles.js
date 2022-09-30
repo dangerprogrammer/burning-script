db3a1c2a6d98e62bb564cb036686dbc6c15412d5
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-09-30 14:57:36 -0300

    .

--- burning-codes/burning-styles.js
+++ burning-codes/burning-styles.js
@@ -2,11 +2,25 @@
     const styling = createElem('style', {
         innerHTML: `
         #darkener {
-            background: #000;
+            background: #111;
+        }
+
+        #menuContainer {
+            top: 0;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+
+        .change-skins {
+            display: flex;
+            flex-direction: column;
+            gap: 15px;
+            padding: 15px;
+            height: 100%;
+            border-right: 2px solid #333;
         }
         `
     });
 
     document.head.append(styling);
 })();
-//pog
