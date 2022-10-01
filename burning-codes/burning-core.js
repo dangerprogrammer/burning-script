5f18ea96f22feb105eff8637a97b3ec5716e5965
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-10-01 00:21:13 -0300

    .

--- burning-codes/burning-core.js
+++ burning-codes/burning-core.js
@@ -10,6 +10,8 @@ while (skinsContainer.length < playerSkins) {
     const skinContainer = createElem('div', {className: 'skin-container'}),
         skinContent = createElem('div', {className: 'skin-content'});
 
+    skinContent.backgroundImage = `url('http://bloble.io/img/skins/skin_${skinsContainer.length}.png')`;
+
     skinContainer.append(skinContent);
     skinsContainer.push(skinContainer);
 };
