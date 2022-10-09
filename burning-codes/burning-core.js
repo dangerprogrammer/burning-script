7f6b1f83c1e42947d67138d60fe491e8301383b4
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2022-10-08 22:45:18 -0300

    .

--- burning-codes/burning-core.js
+++ burning-codes/burning-core.js
@@ -56,8 +56,8 @@ skinsSettingsContainer.append(toggleTypeSkins, toggleSkins, skinsListContainer);
 mainUserSettings.append(usernameplayContainer, skinsSettingsContainer);
 userSettingsContainer.append(gameTitle, mainUserSettings, creditsContainer);
 
-let pagesListLength = Math.ceil(window.innerWidth / 60);
-const gameSkinsPagesLength = Math.ceil(playerSkins / pagesListLength),
+const pagesListLength = 15,
+      gameSkinsPagesLength = Math.ceil(playerSkins / pagesListLength),
       scriptSkinsPagesLength = Math.ceil(featuredSkins / pagesListLength);
 
 let gameSkinsPagesList = [], gameSkinsPageList = [], gamePagesSkin = [], lastGameSkin;
